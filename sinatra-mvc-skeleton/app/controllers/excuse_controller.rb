3d93520690b67b0a0b81990408378d06c4322642
Nicolette Chambers <nicolette3883@gmail.com>
2016-02-04 13:41:49 -0600

    Add ability to create new excuses if logged in

--- sinatra-mvc-skeleton/app/controllers/excuse_controller.rb
+++ sinatra-mvc-skeleton/app/controllers/excuse_controller.rb
@@ -2,3 +2,20 @@ get "/excuses" do
  @excuses = Excuse.all
   erb :"excuses/index"
 end
+
+get "/excuses/new" do
+  erb :"excuses/new"
+end
+
+post "/excuses" do
+  excuse = Excuse.new(body: params[:body],
+                      creator: current_user)
+
+  if excuse.valid?
+    excuse.save
+    redirect "/excuses"
+  else
+   @errors = excuse.errors.full_messages
+   erb :"excuses/new"
+  end
+end
