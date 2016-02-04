aefc34cda1dbb3b0111cc2624f7726bf0b73135b
Nicolette Chambers <nicolette3883@gmail.com>
2016-02-04 15:04:45 -0600

    Add ability to edit and delete excuses

--- sinatra-mvc-skeleton/app/controllers/excuse_controller.rb
+++ sinatra-mvc-skeleton/app/controllers/excuse_controller.rb
@@ -19,3 +19,35 @@ post "/excuses" do
    erb :"excuses/new"
   end
 end
+
+get "/excuses/:id" do
+  # not_authorized
+  @excuse = Excuse.find(params[:id])
+  erb :"excuses/show"
+end
+
+get "/excuses/:id/edit" do
+  # not_authorized
+  @excuse = Excuse.find(params[:id])
+  erb :"excuses/edit"
+end
+
+put "/excuses/:id" do
+  # not_authorized
+  @excuse = Excuse.find(params[:id])
+  @excuse.assign_attributes(body: params[:body])
+
+  if @excuse.valid?
+    @excuse.save
+    redirect "/excuses/#{@excuse.id}"
+  else
+    @errors = @excuse.errors.full_messages
+    erb :"excuses/edit"
+  end
+end
+
+delete "/excuses/:id" do
+  # not_authorized
+  Excuse.find(params[:id]).destroy
+  redirect "/excuses"
+end
